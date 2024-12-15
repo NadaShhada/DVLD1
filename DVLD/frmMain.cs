fe43e178206a607a64d3298c43effee22bab733d
NadaShhada <90151528+NadaShhada@users.noreply.github.com>
2024-12-15 20:48:13 +0300

    Update frmMain.cs

--- DVLD/frmMain.cs
+++ DVLD/frmMain.cs
@@ -1,11 +1,20 @@
-﻿using System;
-using System.Windows.Forms;
-using DVLD.ApplcationsTypes;
+using DVLD.Applications;
+using DVLD.Applications.Detain_License;
+using DVLD.Applications.International_License;
+using DVLD.Applications.ReplaceLostOrDamagedLicense;
+using DVLD.Applications.Rlease_Detained_License;
 using DVLD.Classes;
-using DVLD.Login_Screen;
+using DVLD.Classesp;
+using DVLD.Drivers;
+using DVLD.Licenses;
+using DVLD.Licenses.International_License;
+using DVLD.Login;
 using DVLD.People;
-using DVLD.TestTypes;
+using DVLD.Tests;
 using DVLD.User;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
 
 
 namespace DVLD
@@ -13,15 +22,19 @@ namespace DVLD
 
     public partial class frmMain : Form
     {
-        frmLoginScreen _frmLogin;
-        public frmMain(frmLoginScreen frm)
+        frmLogin _frmLogin;
+
+        public frmMain( frmLogin frm )
         {
             InitializeComponent();
-            _frmLogin = frm;
+            _frmLogin= frm;
 
         }
+
         private void localLicenseToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            frmAddUpdateLocalDrivingLicesnseApplication frm = new frmAddUpdateLocalDrivingLicesnseApplication();
+            frm.ShowDialog();
         }
 
         private void peopleToolStripMenuItem_Click(object sender, EventArgs e)
@@ -30,41 +43,25 @@ namespace DVLD
             frm.ShowDialog();
         }
 
-
-
-       
-
-
-
-
-
-
-
-
-
-
-
-        private void vehiclesLicensesServicesToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("This Feature Is Not Implemented Yet!", "Not Ready", MessageBoxButtons.OK, MessageBoxIcon.Information);
-        }
-
         private void employeesToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            Form frm = new frmListUser();
+            Form frm = new frmListUsers();
             frm.ShowDialog();
         }
 
-        private void changePasswordToolStripMenuItem_Click(object sender, EventArgs e)
+        private void frmMain_Load(object sender, EventArgs e)
         {
-            Form frm = new frmChangePassword(clsGlobal.CurrentUser.UserID);
-            frm.ShowDialog();
+            this.BackColor = Color.White;
+            lblLoggedInUser.Text = "LoggedIn User: " + clsGlobal.CurrentUser.UserName;
+            this.Refresh();
+
         }
 
         private void currentUserInfoToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            Form frm = new frmUserInfo(clsGlobal.CurrentUser.UserID);
+            frmUserInfo frm = new frmUserInfo(clsGlobal.CurrentUser.UserID);
             frm.ShowDialog();
+
         }
 
         private void signOutToolStripMenuItem_Click(object sender, EventArgs e)
@@ -74,16 +71,110 @@ namespace DVLD
             this.Close();
         }
 
+        private void changePasswordToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmChangePassword frm = new frmChangePassword(clsGlobal.CurrentUser.UserID);
+            frm.ShowDialog();
+
+        }
+
         private void manageApplicationTypesToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            Form frm = new frmListApplcationsTypes();
+            frmManageApplicationTypes frm = new frmManageApplicationTypes();
             frm.ShowDialog();
         }
 
         private void manageTestTypesToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            Form frm = new frmListTestTypes();
+            frmListTestTypes frm = new frmListTestTypes();
             frm.ShowDialog();
+        }
+
+        private void internationalLicenseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            frmNewInternationalLicenseApplication frm = new frmNewInternationalLicenseApplication();
+            frm.ShowDialog();
+
+        }
+
+        private void renewDrivingLicenseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmRenewLocalDrivingLicenseApplication frm = new frmRenewLocalDrivingLicenseApplication();
+            frm.ShowDialog();
+
+        }
+
+       
+
+        private void releaseDetainedDrivingLicenseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            frmReleaseDetainedLicenseApplication frm = new frmReleaseDetainedLicenseApplication();
+            frm.ShowDialog();
+        }
+
+        private void retakeTestToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            frmListLocalDrivingLicesnseApplications frm = new frmListLocalDrivingLicesnseApplications();
+            frm.ShowDialog();
+        }
+
+      
+        private void vehiclesLicensesServicesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This Feature Is Not Implemented Yet!", "Not Ready", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void manageLocalDrivingLicenseApplicationsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmListLocalDrivingLicesnseApplications frm = new frmListLocalDrivingLicesnseApplications();
+            frm.ShowDialog();
+
+        }
+
+        private void driversToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmListDrivers frm = new frmListDrivers();
+            frm.ShowDialog();
+
+        }
+
+      
+
+        private void ManageInternationaDrivingLicenseToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            frmListInternationalLicesnseApplications frm = new frmListInternationalLicesnseApplications();
+            frm.ShowDialog();
+
+        }
+
+        private void ReplacementLostOrDamagedDrivingLicenseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmReplaceLostOrDamagedLicenseApplication frm = new frmReplaceLostOrDamagedLicenseApplication();
+            frm.ShowDialog();
+
+        }
+
+        private void ManageDetainedLicensestoolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            frmListDetainedLicenses frm = new frmListDetainedLicenses();
+            frm.ShowDialog();
+
+        }
+
+        private void detainLicenseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           frmDetainLicenseApplication frm = new frmDetainLicenseApplication();
+            frm.ShowDialog();
+
+        }
+
+        private void releaseDetainedLicenseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmReleaseDetainedLicenseApplication frm= new frmReleaseDetainedLicenseApplication();   
+            frm.ShowDialog();
+
         }
     }
 }
