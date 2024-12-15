b4b5448deb828f60ce8e71cdc4820a498ea8748e
NadaShhada <90151528+NadaShhada@users.noreply.github.com>
2024-12-15 20:52:09 +0300

    Delete DVLD_DataAccess directory

--- DVLD_DataAccess/clsDataAccessSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DVLD_DataAccess
-{
-  public  static class clsDataAccessSettings
-    {
-        public static string ConnectionString = "Server=.;Database=DVLD;User Id=sa;Password=sa123456;";
-
-
-    }
-}
